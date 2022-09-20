027541ec7c4974da0267fef3b4db0f90448fe01e
Hasanat902 <abulhasanat902@gmail.com>
2022-09-20 17:29:00 +0600

    added footer

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import chair from '../../assets/images/chair.png';
-import bg from '../../assets/images/bg.png';
-import PrimaryButton from '../Shared/PrimaryButton';
+import React from "react";
+import chair from "../../assets/images/chair.png";
+import bg from "../../assets/images/bg.png";
+import PrimaryButton from "../Shared/PrimaryButton";
 
 const Banner = () => {
-    return (
-        <div style={{background: `url(${bg})`, backgroundSize: 'cover'}} class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='' />
-                <div>
-                <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi.<br></br> In deleniti eaque aut repudiandae et a id nisi.</p>
-                <PrimaryButton>Get Started</PrimaryButton>
-                </div>
-            </div>
+  return (
+    <div
+      style={{ background: `url(${bg})`, backgroundSize: "cover" }}
+      className="hero min-h-screen"
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
+        <div>
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi.<br></br> In deleniti eaque aut
+            repudiandae et a id nisi.
+          </p>
+          <PrimaryButton>Get Started</PrimaryButton>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
