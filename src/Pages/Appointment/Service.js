1cfd74e633a139a6b70d0535e3637deea0c26548
Hasanat902 <abulhasanat902@gmail.com>
2022-09-22 15:26:25 +0600

    implement google sign in

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,18 +4,29 @@ const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
 
   return (
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body text-center">
-        <h2 class="text-xl font-bold text-secondary">{name}</h2>
-        <p>{
-            slots.length > 0
-            ? <span>{slots[0]}</span>
-            : <span className="text-red-500">Try another date</span>
-            }</p>
-        <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-        <div class="card-actions justify-center">
-          <label for="booking-modal" disabled={slots.length === 0} onClick={() => setTreatment(service)} class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
-        </div> 
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body text-center">
+        <h2 className="text-xl font-bold text-secondary">{name}</h2>
+        <p>
+          {slots.length > 0 ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-red-500">Try another date</span>
+          )}
+        </p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <div className="card-actions justify-center">
+          <label
+            for="booking-modal"
+            disabled={slots.length === 0}
+            onClick={() => setTreatment(service)}
+            className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
+          >
+            Book Appointment
+          </label>
+        </div>
       </div>
     </div>
   );
