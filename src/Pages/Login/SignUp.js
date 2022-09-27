d86a5bf61612ab5a2085ba7be0d14a59b62de648
Hasanat902 <abulhasanat902@gmail.com>
2022-09-27 12:55:18 +0600

    save user information to the database

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -8,6 +8,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useNavigate } from "react-router-dom";
+import useToken from "../../hooks/useToken";
 
 const SignUp = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -16,6 +17,7 @@ const SignUp = () => {
 
   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
   const navigate = useNavigate();
+  const [token] = useToken(user || gUser);
 
   const {
     register,
@@ -39,15 +41,16 @@ const SignUp = () => {
     );
   }
 
-  if (user || gUser) {
-    console.log(user || gUser);
+  if (token) {
+  
+    navigate("/appointment");
   }
 
   const onSubmit = async (data) => {
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
     console.log("Update Done");
-    navigate("/appointment");
+    
   };
 
   return (
